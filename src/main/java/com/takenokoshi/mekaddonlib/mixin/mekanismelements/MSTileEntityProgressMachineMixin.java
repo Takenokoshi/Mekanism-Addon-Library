222845eb1cff41c7904532f14078ad7624eca10c
Takenokoshi <takenokoshichannel@gmail.com>
2026-08-10 18:00:32 +0900

    v1.0.4-adapt MekElements change

--- src/main/java/com/takenokoshi/mekaddonlib/mixin/mekanismelements/MSTileEntityProgressMachineMixin.java
+++ src/main/java/com/takenokoshi/mekaddonlib/mixin/mekanismelements/MSTileEntityProgressMachineMixin.java
@@ -4,7 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.fxd927.mekanismelements.common.tile.prefab.MSTileEntityProgressMachine;
+import fixdol.mekanismelements.common.tile.prefab.MSTileEntityProgressMachine;
 import com.takenokoshi.mekaddonlib.mixin.mekanism.tile.TileEntityMekanismMixin;
 import com.takenokoshi.mekaddonlib.upgrade.AdditionalUpgradeUtils;
 
--- src/main/java/com/takenokoshi/mekaddonlib/mixin/mekanismelements/TileEntityElementsResourceGeneratorMachinesMixin.java
+++ src/main/java/com/takenokoshi/mekaddonlib/mixin/mekanismelements/TileEntityElementsResourceGeneratorMachinesMixin.java
@@ -4,8 +4,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.fxd927.mekanismelements.common.tile.machine.TileEntityAirCompressor;
-import com.fxd927.mekanismelements.common.tile.machine.TileEntitySeawaterPump;
+import fixdol.mekanismelements.common.tile.machine.TileEntityAirCompressor;
+import fixdol.mekanismelements.common.tile.machine.TileEntitySeawaterPump;
 import com.takenokoshi.mekaddonlib.mixin.mekanism.tile.TileEntityMekanismMixin;
 import com.takenokoshi.mekaddonlib.upgrade.AdditionalUpgradeUtils;
 
